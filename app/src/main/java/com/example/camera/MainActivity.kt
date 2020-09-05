149f09c370ab7520830a75bc24b772aa8d2c7083
Kashif <kashifqamar111@gmail.com>
2020-09-05 16:56:54 +0300

    High res photos saved and displayed

--- app/src/main/java/com/example/camera/MainActivity.kt
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -4,34 +4,63 @@ import android.app.Activity
 import android.content.ComponentName
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
+import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 99
+    val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var mCurrentPhotoPath: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fileName = "temp_photo"
+        val imgPath = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        var imageFile: File? = null
+        imageFile = File.createTempFile(fileName, ".jpg", imgPath )
+
+         mCurrentPhotoPath = imageFile!!.absolutePath
+
+        val photoURI: Uri = FileProvider.getUriForFile(this, "com.example.camera", imageFile)
+
+        /*val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (myIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
+        }*/
 
 
 
         val myIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (myIntent.resolveActivity(getPackageManager()) != null) {
+        if (myIntent.resolveActivity(packageManager) != null) {
+            myIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            myIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
             startActivityForResult(myIntent, REQUEST_IMAGE_CAPTURE)
         }
 
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
+   /* override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
         super.onActivityResult(requestCode, resultCode, recIntent)
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
             val extras = recIntent!!.extras
             val imageBitmap = extras!!.get("data") as Bitmap
             imageView.setImageBitmap(imageBitmap)
         }
-    }
-}
+    }*/
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, recIntent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, recIntent)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath)
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }}
